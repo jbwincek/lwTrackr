140a71e854f092910e6c4047e85e4374db87e9eb
J B Wincek <jbwincek@gmail.com>
2012-06-27 08:16:09 -0400

    Created skeleton of program
    Added files,

---
+++ Database.py
@@ -0,0 +1,19 @@
+"""
+The database module
+"""
+
+class Database: 
+	
+	def __init__: pass
+	
+	def load(): pass
+	
+	def user_exists(unique_ID): pass
+	
+	def add_entry(unique_ID): pass
+	
+	def modify_entry(unique_ID): pass
+	
+	def close(): pass
+	
+	
