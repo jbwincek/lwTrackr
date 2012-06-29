ccb059b352df5c3214fdd73e49b0e64ed48a0c03
J B Wincek <jbwincek@gmail.com>
2012-06-29 02:22:42 -0400

    Fleshed out code some (see description)
    * Moved Mood.py into the right folder
    * tracker_wrapper.py has a substantial amount of code and comments
    added to it, although many functions still do nothing and need more
    coding.
    * tracker_control_flow_outline.txt is updated to make the third block
    of pseudo code much more like actual code, nearly ready for copying
    into a python file.
    * Database.py has additional comments, and some preliminary code added.

--- Database.py
+++ Database.py
@@ -1,18 +1,50 @@
+import mood as IM
 """
 The database module
+The database class represents contains all of the users and all of their entries
+
+Currently the plan is to store everything in a dictionary using the unique_ID as a key
+	and the value as a instance of the Mood class.
+
+This file should also include methods for user input and display
+	eventually just import those functions from somewhere
 """
 
 class Database: 
 	
-	def __init__: pass
+	def __init__: 
+		data = []
+		file="tracker_data.jbw"
 	
-	def load(): pass
+	def load(file_choice = file): 
+		"""
+		load data from a file using default filename unless specified 
+		Test how this works with different paths and different running methods (ie cron jobs)
+		"""
+		if file_choice!=file:
+			file = file_choice
+
+		pass
 	
-	def user_exists(unique_ID): pass
+	def user_exists(unique_ID):
+		"""
+		Boolean function checking to see if the user exists
+		"""
+		if not data[unique_ID]:
+			return True
+		else: 
+			return False 
+		
 	
 	def add_entry(unique_ID): pass
 	
 	def modify_entry(unique_ID): pass
+	
+	def delete_entry(unique_ID): pass
+	
+	def save(file_choice= file):
+		if file_choice!=file:
+			file = file_choice
 	
 	def close(): pass
 	
--- Tracker_wrapper.py
+++ Tracker_wrapper.py
@@ -9,10 +9,50 @@ greeting():
 
 """
 
-def greeting(): pass
+def greeting(): 
+	print( """
+Welcome to the tracker app version: %g
+By J B Wincek """ % (versionNumber))
 
-def parse_input_for_( action): pass
+def parse_input_from_( action): 
+	"""
+	This is currently a stand in function
+	NEEDS to be updated to actually parse input
+	call deal_with_poor_choice() if appropriate
+	takes an unclean input as a number
+	returns a string for the actions 
+	"""
+	return action
 
-def deal_with_poor_choice(): pass
+def deal_with_poor_choice():
+	print("Your choice is not a valid option, please type just the number then hit enter")
 
 def close_application(): pass
+
+def get_ID(unique_ID):
+	unique_ID = input("Please enter your unique user identifier.\nIf you are a new user, pick an ID (a few letters) and enter that.")
+	return unique_ID
+
+def display_action_menu():
+	"""
+	This action menu could be updated to be represented as a list of strings for each action
+		that would allow easier adding and indexing of actions to help make parse_input_from()
+		more robust
+	"""
+	print ("""
+~-~-~-~   Main menu:   ~-~-~-~ 
+Available Actions:
+	1.) Display all the entries
+	2.) Add a new entry
+	3.) Modify an existing entry
+	4.) Delete an entry
+	5.) Quit the program
+""")
+
+def get_action():
+	unclean_action = input("Please enter the number corrosponding to your choice of action:")
+	#the action NEEDS to be cleaned up before this can go primetime
+	action = parse_input_from(unclean_action)
+	return action
+	
+	
